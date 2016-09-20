7ffc70ef305980e2ec1d212804fa8361d1cd78ca
Federico Roasio <froasio@oneloop.com.ar>
2016-09-20 18:04:45 -0300

    Agrega digrafos

---
+++ tests/DigraphTest.cpp
@@ -0,0 +1,22 @@
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(digraphTest)
+{
+
+    void setup()
+    {
+
+    }
+
+    void teardown()
+    {
+
+    }
+};
+
+TEST(digraphTest, testRandomInputs)
+{
+	
+	CHECK_EQUAL(true,true);
+
+}
