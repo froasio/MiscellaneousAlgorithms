3fec2e270ea9c71e29a361f974190e6c03bfc314
froasio <federoasio@gmail.com>
2016-12-10 19:34:32 -0300

    Version sin doble espacio

--- src/LinearSorting.cpp
+++ src/LinearSorting.cpp
@@ -16,20 +16,17 @@ void LinearSorting::counting(vector<int> &v) {
 
 	int buckets = max - min + 1;
 	vector<int> count(buckets, 0);
-	vector<int> output(v.size(),0);
 
 	for(int x: v)
 		count[x - min]++;
 
-	for(int i = 1; i < buckets; i++)
-		count[i] += count[i-1];
-
-	for(int i = (int)v.size() - 1; i >= 0 ; i--){
-		output[count[v[i]-min]-1] = v[i];
-		count[v[i]-min]--;
+	for(int i = min, z = 0; i <= max; i++)
+	{
+	    while( count[i - min] > 0 )
+	    {
+			v[z++] = i;
+			count[i - min]--;
+	    }
 	}
-
-	for(int i = 0; i < (int)v.size(); i++)
-		v[i] = output[i];
 
 }
